15f08057562b3658a06e83fc9a9ac3f84b8cb04c
MichaelTimmers6 <97582970+MichaelTimmers6@users.noreply.github.com>
2025-01-22 19:29:15 +1000

    Create another recursive definition of bubble sort with sub recursion

--- algorithms/bubble-sort.scm
+++ algorithms/bubble-sort.scm
@@ -2,8 +2,14 @@
 #:export (bubble-sort))
 
 (define (bubble-sort lst)
+    (if (null? lst)
+        '()
+        (let* ((lst-rev (reverse lst)))
+            (cons (car lst-rev) (bubble-sort (single-buble (cdr rev-list)))))))
+
+(define (single-bubble lst)
     (cond ((null? (cdr lst))
             (cons (car lst) '()))
         ((> (car lst) (cadr lst))
-            (cons (cadr lst) (bubble-sort (cons (car lst) (cddr lst))))) ;;swap the current and next elements
-        (else (cons (car lst) (bubble-sort (cdr lst)))))) ;;don't swap and continue
+            (cons (cadr lst) (single-bubble (cons (car lst) (cddr lst))))) ;;swap the current and next elements
+        (else (cons (car lst) (single-bubble (cdr lst)))))) ;;don't swap and continue
