a822e1b61b1caaaf5c7f0502ab074935ebe8c062
MichaelTimmers6 <97582970+MichaelTimmers6@users.noreply.github.com>
2025-01-23 15:28:14 +1000

    Changed take to car so that new list aren't created in cons

--- algorithms/merge-sort.scm
+++ algorithms/merge-sort.scm
@@ -19,13 +19,13 @@ lst
     (cond ((null? l1)
             (if (null? (cdr l2))
                 l2 ;;base case  where there is only one item left in l2
-                (cons(take l2 1) (merge-lists l1 (drop l2 1))))) ;;take from l2
+                (cons(car l2) (merge-lists l1 (drop l2 1))))) ;;take from l2
         ((null? l2)
             (if (null? (cdr l1))
                 l1 ;; base case where there is only one item in l1 left
-                (cons (take l1 1) (merge-lists (drop l1 1) l2)))) ;;take from l1
+                (cons (car l1) (merge-lists (drop l1 1) l2)))) ;;take from l1
         ((< (car l1) (car l2))
-            (cons (take l1 1) (merge-lists (drop l1 1) l2))) ;;take from l1
+            (cons (car l1) (merge-lists (drop l1 1) l2))) ;;take from l1
         (else
-            (cons (take l2 1) (merge-lists l1 (drop l2 1)))) ;;take from l2
+            (cons (car l2) (merge-lists l1 (drop l2 1)))) ;;take from l2
         ))
