52856feeeac7a2f695d2d845a5c216a0287319f8
MichaelTimmers6 <97582970+MichaelTimmers6@users.noreply.github.com>
2025-01-23 15:15:20 +1000

    Changed from number? to null  check of cdr

--- algorithms/merge-sort.scm
+++ algorithms/merge-sort.scm
@@ -17,11 +17,11 @@ lst
 
 (define (merge-lists l1 l2)
     (cond ((null? l1)
-            (if (number? l2)
+            (if (null? (cdr l2))
                 l2 ;;base case  where there is only one item left in l2
                 (cons(take l2 1) (merge-lists l1 (drop l2 1))))) ;;take from l2
         ((null? l2)
-            (if (number? l1)
+            (if (null? (cdr l1))
                 l1 ;; base case where there is only one item in l1 left
                 (cons (take l1 1) (merge-lists (drop l1 1) l2)))) ;;take from l1
         ((< (car l1) (car l2))
